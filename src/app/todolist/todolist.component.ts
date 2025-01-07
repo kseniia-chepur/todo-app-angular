511c566d2558c68cf4b3c0dcde7de5f3581da7d1
Kseniia Chepur <kseniia.chepur@gmail.com>
2025-01-07 14:24:43 +0200

    add task interface, implement storing data to localStorage

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,55 +1,90 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { Task } from '../interface/task';
 
 @Component({
   selector: 'app-todolist',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+  ],
   templateUrl: './todolist.component.html',
   styleUrl: './todolist.component.scss',
 })
-export class TodolistComponent {
-  tasks = [
-    { description: 'first task', isCompleted: false, isEditable: false },
-    { description: 'second task', isCompleted: true, isEditable: false },
+export class TodolistComponent implements OnInit {
+  tasks: Task[] = [];
+  displayedColumns: string[] = [
+    'description',
+    'isCompleted',
+    'Edit',
+    'Save',
+    'Delete',
   ];
+  dataSource: MatTableDataSource<Task>;
 
-  displayedColumns: string[] = ['description', 'isCompleted', 'Edit', 'Save', 'Delete'];
-  dataSource = new MatTableDataSource(this.tasks);
+  ngOnInit(): void {
+    this.getFromLocalStorage();
+    this.dataSource = new MatTableDataSource(this.tasks);
+  }
+
+  getFromLocalStorage() {
+    const tasksFromLocalStorage = localStorage.getItem('tasks');
+    if (tasksFromLocalStorage) {
+      this.tasks = JSON.parse(tasksFromLocalStorage);
+    }
+  }
+
+  storeInLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
 
   handleSubmit(form: NgForm) {
-     this.tasks.push({
+    this.tasks.push({
       description: form.controls['task'].value,
       isCompleted: false,
       isEditable: false,
     });
 
-     this.dataSource._updateChangeSubscription();
+    this.dataSource._updateChangeSubscription();
+    this.storeInLocalStorage();
 
     form.reset();
   }
 
-  handleDelete(index: number) {
-    this.tasks.splice(index, 1);
-    this.dataSource._updateChangeSubscription();
-  }
-
   handleStatusChange(index: number, status: boolean) {
     this.tasks[index].isCompleted = status;
+
+    this.storeInLocalStorage();
   }
 
   handleEdit(index: number) {
     this.tasks[index].isEditable = true;
+
+    this.storeInLocalStorage();
   }
 
   handleSave(index: number) {
     this.tasks[index].isEditable = false;
+
+    this.storeInLocalStorage();
+  }
+
+  handleDelete(index: number) {
+    this.tasks.splice(index, 1);
+
     this.dataSource._updateChangeSubscription();
+    this.storeInLocalStorage();
   }
 }
