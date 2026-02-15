7785b01b06c6bc2b1e7b2f8c56cbec33c1f798d3
Kseniia Chepur <kseniia.chepur@gmail.com>
2026-02-15 15:52:04 +0200

    feat: add footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TodolistComponent } from './todolist/todolist.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [TodolistComponent],
+  imports: [TodolistComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
